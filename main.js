e0c156908214ebe48bba1ce3f0c686be5e244e99
Ballthoven <ballthovenkm@gmail.com>
2026-02-08 22:10:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function moveup(){
 //That your counter in the Food stuff it had negative values
 let quantsInputs = document.querySelectorAll('.quant');
 quantsInputs.forEach((input)=>{
-    input.addEventListner('input', => {
+    input.addEventListner('input', ()=> {
         input.value = Math.max(0, input.value);
     });
 });
