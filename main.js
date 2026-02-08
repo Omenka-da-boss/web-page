546a16bf3cae84bc759495f3e5082280b24de7ea
Ballthoven <ballthovenkm@gmail.com>
2026-02-08 21:29:53 +0100

    Add validation to prevent negative quantity inputs
    Prevent negative values in quantity inputs by setting them to zero.

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const container = document.querySelector('.container')
 function moveup(){
     window.scrollTo({top:0,behavior:'smooth'})
 }
+
+//That your counter in the Food stuff it had negative values
+let quantsInputs = document.querySelectorAll('.quant');
+quantsInputs.forEach((input)=>{
+    input.addEventListner('input', => {
+        input.value = Math.max(0, input.value);
+    });
+});
